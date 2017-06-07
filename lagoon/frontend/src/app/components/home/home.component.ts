f8e5b371fccf2035f27f4d721bd703d37a1b9ca2
zhenglingfeng <bszhenglf@163.com>
2017-06-07 17:23:35 +0800

    项目整理

--- lagoon/frontend/src/app/components/home/home.component.ts
+++ lagoon/frontend/src/app/components/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var $: any;
+
 @Component({
   selector: 'home',
   templateUrl: './home.component.html',
@@ -10,6 +12,7 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    console.log($);
   }
 
 }
